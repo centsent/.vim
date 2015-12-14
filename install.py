96c182a7209698e0e88aa4f151f4c0955159587e
Centsent <centsent@users.noreply.github.com>
2015-12-15 04:08:15 +0800

    chore: do not need vundle settings any more

--- install.py
+++ install.py
@@ -9,10 +9,6 @@ HOME = path.expanduser('~')
 GIT = 'git'
 CLONE = 'clone'
 
-# vundle
-VUNDLE_HOME = '%s/.vim/bundle/Vundle.vim' % (HOME)
-VUNDLE_HOME_GIT = '%s/.git' % (VUNDLE_HOME)
-
 # vim
 VIM_SOURCE = '%s/.vim' % (HOME)
 VIMRC_SOURCE = '%s/.vimrc' % (HOME)
@@ -21,29 +17,8 @@ VIMRC_BAK_DEST = '%s.bak' % (VIMRC_SOURCE)
 VIMRC_SOFT_LINK = ['ln', '-s', '%s/.vim/.vimrc' % (HOME), VIMRC_SOURCE]
 
 # repo
-VUNDLE_REPO = 'https://github.com/VundleVim/Vundle.vim.git'
-CLONE_VUNDLE_REPO = [GIT, CLONE, VUNDLE_REPO, VUNDLE_HOME]
 MY_REPO = 'https://github.com/centsent/vimrc.git'
 CLONE_REPO = [GIT, CLONE, MY_REPO, VIM_SOURCE]
-
-
-def vundle_init():
-    # check ~/.vim/bundle/Vundle.vim
-    if not path.exists(VUNDLE_HOME):
-        call(['mkdir', '-p', VUNDLE_HOME])
-
-    # check ~/.vim/bundle/Vundle.vim
-    if not path.exists(VUNDLE_HOME_GIT):
-        call(CLONE_VUNDLE_REPO)
-        print '''
-        Read about vim configuration for vundle at
-        https://github.com/VundleVim/Vundle.vim
-        '''
-
-
-def vundle_install_plugin():
-    call(['vim', '-c', '"execute \"PluginInstall\" | qa"'])
-
 
 def soft_link_vimrc():
     call(VIMRC_SOFT_LINK)
@@ -58,10 +33,6 @@ def main():
         shutil.move(VIMRC_SOURCE, VIMRC_BAK_DEST)
     # clone the git repo
     call(CLONE_REPO)
-    # vundle init
-    vundle_init()
-    # install vim plugins
-    vundle_install_plugin()
     # soft link to vimrc
     soft_link_vimrc()
     # TODO install YouCompleteMe
